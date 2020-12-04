613195c358b0708048b92e249138b07759dd9134
shawn220a <shawn.42496@gmail.com>
2020-12-04 09:15:45 -0600

    Add xcode script to install shell

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@
 
 echo "Setting up your new Mac Home..."
 
+# Uninstall and Install xcode tools
+softwareupdate --all --install --force
+sudo xcode-select --install
+
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"

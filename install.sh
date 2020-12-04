25f2406b1705eb1b7ef48e3c4b7eb4121d50e0a1
shawn220a <shawn.42496@gmail.com>
2020-12-03 21:53:55 -0600

    Create basic intall.sh scripts

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo "Setting up your new Mac Home..."
+
+# Check for Homebrew and install if we don't have it
+if test ! $(which brew); then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update Homebrew recipes
+brew update
+
+# Install all our dependencies with bundle (See Brewfile)
+brew tap homebrew/bundle
+brew bundle
+
+# Make BASH the default shell environment
+chsh -s $(which bash)

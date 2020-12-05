48419c506578fdb9e84d9922d100b40150d33998
shawn220a <shawn.42496@gmail.com>
2020-12-05 00:26:01 -0600

    Add install script for Cobalt2 theme

--- fonts.sh
+++ fonts.sh
@@ -8,3 +8,8 @@ git clone https://github.com/powerline/fonts.git
 cd fonts
 ./install.sh
 trash fonts
+
+# Installing Cobalt2 by Wes Bos
+cd ~
+git clone https://github.com/wesbos/Cobalt2-iterm.git
+cp -R Cobalt2-iterm/cobalt2.zsh-theme ~/.oh-my-zsh/themes

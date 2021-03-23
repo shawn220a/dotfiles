99e968e58f731adf574c3a7878b766ecf3733f9e
shawn220a <shawn.42496@gmail.com>
2021-03-22 19:21:14 -0500

    Add instruction for windows install

---
+++ windows.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "Setting up your new Windows home ..."
+
+# Chocolatey Install
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+# Chocolatey Apps
+choco install steam -y
+choco install git -y
+choco install vscode-insiders -y
+choco install discord -y
+choco install unity -y
+choco install python -y
+choco install gh -y
